16a49ada8bbc59df015ae092fac6ef441ca83509
V1dhun <vidhunedl@gmail.com>
2019-12-24 14:22:46 +0530

    Update run.sh

--- Using-Components-with-Known-Vulnerabilities/run.sh
+++ Using-Components-with-Known-Vulnerabilities/run.sh
@@ -2,7 +2,7 @@ if ! command -v docker > /dev/null; then
    echo "Docker is !installed"
    exit
 fi
-bash ./bin/gdrivedl "https://drive.google.com/uc?id=11UiEawLb8GB5-VLHyoE7n2F97HOyBYsp&export=download" "tomcat.tar"
+bash ./bin/gdrivedl "https://drive.google.com/uc?id=11UiEawLb8GB5-VLHyoE7n2F97HOyBYsp&export=download" "tomcat.tar" # 150 Mb 
 if [[ -e FILE ]];then
 	sudo docker load < tomcat.tar
 	sudo docker run -p 8000:8080 tomcat
