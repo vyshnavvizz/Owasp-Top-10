5b4c80585b96e3ef8b970278c0938a3456549a97
vidhun <vidhunedl@gmail.com>
2019-12-24 14:19:45 +0530

    Updating Using-Components-with-Known-Vulnerabilities

--- Using-Components-with-Known-Vulnerabilities/run.sh
+++ Using-Components-with-Known-Vulnerabilities/run.sh
@@ -2,5 +2,9 @@ if ! command -v docker > /dev/null; then
    echo "Docker is !installed"
    exit
 fi
-sudo docker load < tomcat.tar
-sudo docker run -p 8000:8080 tomcat
+bash ./bin/gdrivedl "https://drive.google.com/uc?id=11UiEawLb8GB5-VLHyoE7n2F97HOyBYsp&export=download" "tomcat.tar"
+if [[ -e FILE ]];then
+	sudo docker load < tomcat.tar
+	sudo docker run -p 8000:8080 tomcat
+fi
+   echo "Something Went Wrong"
